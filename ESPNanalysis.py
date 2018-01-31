3b44d78f2a09fbf8c7cc330a61eb6e55fc83458e
Alexander Hill <35545251+McFloof@users.noreply.github.com>
2018-01-31 12:43:16 -0700

    Add files via upload

---
+++ ESPNanalysis.py
@@ -0,0 +1,21 @@
+# This program takes the website retrieved by webscrapeESPN and extracts data with regex
+
+import re
+
+with open('espnrankings.txt', 'r') as espn_file:
+    data = espn_file.read()
+    result1 = re.findall('<p><b>(.*?)</b><br />(.*?)</p><p><inline.*?-wide></p><p>(.*?)<em>(.*?)</em>', data, re.DOTALL)
+    result2 = re.findall('<p><b>(.*?)</b><br />.*?</p><p><inline.*?-wide></p><p>.*?<em>.*?</em>', data, re.DOTALL)
+    result3 = re.findall('<p><b>.*?</b><br />(.*?)</p><p><inline.*?-wide></p><p>.*?<em>.*?</em>', data, re.DOTALL)
+    result4 = re.findall('<p><b>.*?</b><br />.*?</p><p><inline.*?-wide></p><p>(.*?)<em>.*?</em>', data, re.DOTALL)
+    result5 = re.findall('<p><b>.*?</b><br />.*?</p><p><inline.*?-wide></p><p>.*?<em>(.*?)</em>', data, re.DOTALL)
+
+with open('espn_final.txt', 'w') as final_espn:
+	print(result1, file=final_espn)
+        
+with open('espn_final.txt', 'a') as final_espn:
+    print(result2, file=final_espn)
+    print(result3, file=final_espn)
+    print(result4, file=final_espn)
+    print(result5, file=final_espn)
+print()
